c5cf2bea3fa461df9c650ed2ed51674f6fac6016
Rajendar <rajendarsagar2000@gmail.com>
2026-01-26 22:03:16 +0530

    tests/example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,13 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://solvative.com');
-  await expect(page).toHaveTitle(
-    'Solvative | Driving Innovation with Full-Stack Solutions'
-  );
+test.describe('solvative homepage', () => {
+  test.use({baseURL: 'https://solvative.com'});
+
+  test('has title', async ({ page }) => {
+    await page.goto('/careers');
+    await expect(page).toHaveTitle(
+      'Solvative | Driving Innovation with Full-Stack Solutions'
+    );
+  });
 });
