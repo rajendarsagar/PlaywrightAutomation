c680eb6c245f5bedee20fe2084c0c2e97a374f38
Rajendar <rajendarsagar2000@gmail.com>
2026-03-06 19:20:47 +0530

    ScreenShot Fix

--- tests/Screenshot.spec.js
+++ tests/Screenshot.spec.js
@@ -1,9 +1,11 @@
 const { test, expect } = require('@playwright/test');
+const path = require('path');
 
 test('Full Page Screenshot', async ({ page }) => {
     await page.goto('https://www.amazon.in/');
-    await expect(page.getByLabel('Amazon.in', { exact: true })).toBeVisible({ timeout: 5000 });
+    await expect(page.getByLabel('Amazon.in', { exact: true })).toBeVisible();
     //await page.waitForLoadState('domcontentloaded',{timeout:5000});
-    await page.screenshot({ path: 'Screenshots/Fullpage.png', timeout: 6000 });
+    const filepath = path.join(__dirname, "../Screenshots/Fullpage.png");
+    await page.screenshot({ path: filepath, timeout: 6000 });
     //await page.screenshot({ path: 'Screenshots/Fullpage.png', fullPage: true, maskColor: 'red', timeout: 5000 });
 });
