e6f4fb7e4abc7c879c72206deab705c55ca2cd17
Rajendar <rajendarsagar2000@gmail.com>
2026-03-14 18:59:20 +0530

    fixes

--- tests/APIAbort.spec.js
+++ tests/APIAbort.spec.js
@@ -3,8 +3,13 @@ const {test, expect} = require('@playwright/test');
 test('Abort the Images and Css', async({page})=>{
 
     
-    await page.route('**/*.{png,jpg,jpeg,css}',route=> route.abort());
+    //await page.route('**/*.{png,jpg,jpeg,css}',route=> route.abort());
+    
+    page.on('response', response => {
+        console.log(response.url(), response.status());
+    });
     await page.goto('https://solvative.com/');
+    await page.waitForLoadState('networkidle');
 
-    await page.pause();
+    //await page.pause();
 })
