c36ca53b453d24396f2b5115a7d8c3554ecc31fb
Rajendar <rajendarsagar2000@gmail.com>
2026-03-21 21:10:06 +0530

    Latest Changes

--- tests/APIAbort.spec.js
+++ tests/APIAbort.spec.js
@@ -2,14 +2,38 @@ const {test, expect} = require('@playwright/test');
 
 test('Abort the Images and Css', async({page})=>{
 
+    let successAPIList = [];
+    let failedAPIList = [];
+    let tokenFieldAPIList = [];
     
     //await page.route('**/*.{png,jpg,jpeg,css}',route=> route.abort());
     
-    page.on('response', response => {
-        console.log(response.url(), response.status());
-    });
+    page.on('response', async response => {
+    const contentType = response.headers()['content-type'] || '';
+
+    if (response.status() < 400) {
+        successAPIList.push(response.url());
+
+        if (contentType.includes('application/json')) {
+            try {
+                const jsonResponse = await response.json();
+                if (jsonResponse.token != null) {
+                    tokenFieldAPIList.push(response.url());
+                }
+            } catch (e) {
+                // not valid JSON despite content-type header
+            }
+        }
+    } else {
+        failedAPIList.push(response.url());
+    }
+});
+
     await page.goto('https://solvative.com/');
-    await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('load');
 
     //await page.pause();
+    console.log('Success API List: ', successAPIList);
+    console.log('Failed API List: ', failedAPIList);
+    console.log('Token Field API List: ', tokenFieldAPIList);
 })
--- tests/FormErrorMessagesValidation.spec.js
+++ tests/FormErrorMessagesValidation.spec.js
@@ -15,8 +15,8 @@ test.describe('Form Error Messages Validation Tests', () => {
         await expect(formValidationInstance.aboutDropdown).toHaveText('Please select what this is about');
         console.log("Form Error Messages Validated Successfully on Home Page");
 
-    }
-    );
+    });
+
     test('Validate Form Error Messages on Contact-Us Page', async ({page}) => {
         const formValidationInstance = new FormValidation(page);
         await page.goto('https://solvative.com/contact-us/');
@@ -29,7 +29,6 @@ test.describe('Form Error Messages Validation Tests', () => {
         await expect(formValidationInstance.aboutDropdown).toHaveText('Please select what this is about');
         console.log("Form Error Messages Validated Successfully on Contact-Us Page");
 
-    }
-
-    );
+    });
+    
 });
